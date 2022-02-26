1a02245a0ea2d1a7b6ecee4f54ffa95bdb048d88
meeba-dev <mikhailbaum@icloud.com>
2022-02-26 05:18:14 +0400

    feat: add towelSort algorithm

--- src/index.js
+++ src/index.js
@@ -2,5 +2,12 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+    if (matrix === undefined || matrix.length == 0) {
+      return [];
+    } else {
+      return matrix.reduce((prev, current, init) => {
+        current.sort((a, b) => !(init & 1) ? a - b : b - a).map(e => prev.push(e));
+        return prev;
+      }, [])
+    }   
 }
